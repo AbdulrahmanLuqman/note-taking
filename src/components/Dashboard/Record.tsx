be9b1b2c2227a4b063a03e453cc1632b5e481480
AbdulHameedi <sanilinity507@gmail.com>
2024-12-13 00:22:17 +0100

    css

--- src/components/Dashboard/Record.tsx
+++ src/components/Dashboard/Record.tsx
@@ -103,7 +103,7 @@ const Record: React.FC = () => {
       </div>
 
       {transcription && (
-        <main className="w-[90%] bg-white h-[400px] rounded-md p-3 border-t-[30px] border-black relative">
+        <main className="w-[90%] bg-white h-[410px] rounded-md p-3 border-t-[30px] border-black relative">
           <p
             className="absolute top-[-27px] right-2 text-black cursor-pointer text-sm bg-gray-100 p-1"
             onClick={() => navigator.clipboard.writeText(transcription)}
