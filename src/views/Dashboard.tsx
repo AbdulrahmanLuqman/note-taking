5b2c7b763986868c9b783ff4d7c40fbfb071c044
AbdulHameedi <sanilinity507@gmail.com>
2024-12-13 00:20:42 +0100

    CSS changes

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -4,7 +4,7 @@ import Mode from "../components/Mode"
 
 const Dashboard = () => {
   return (
-    <div className="text-4xl flex flex-col gap-10 items-center h-screen w-full bg-[whitesmoke] dark:bg-[#0E1117]">
+    <div className="text-4xl flex flex-col gap-10 items-center h-screen overflow-y-scroll py-6 w-full bg-[whitesmoke] dark:bg-[#0E1117]">
       <Mode />
       <Header />
       <SpeakToDocs />
